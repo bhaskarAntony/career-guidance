67809910d923d974d0bf85c9a5a39abd59f4ca1b
bpdeveloperscommunity <bepracticaldeveloperscommunity@gmail.com>
2024-03-19 10:56:26 +0530

    all files

--- src/App.js
+++ src/App.js
@@ -3,8 +3,31 @@ import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home/Home';
 import Header from './components/Header/Header';
+import ReactGA from 'react-ga';
+import { useEffect } from 'react';
+ReactGA.initialize('G-C8KREWK8L7');
 
 function App() {
+  ReactGA.event({
+    category: 'User',
+    action: 'Clicked Button',
+    label: 'Homepage',
+  });
+
+  useEffect(() => {
+    // Extract UTM parameters from the URL
+    const urlParams = new URLSearchParams(window.location.search);
+    const utmSource = urlParams.get('utm_source');
+    const utmMedium = urlParams.get('utm_medium');
+    const utmCampaign = urlParams.get('utm_campaign');
+
+    // Track pageview with UTM parameters
+    ReactGA.pageview(window.location.pathname + window.location.search, [], {
+      utm_source: utmSource,
+      utm_medium: utmMedium,
+      utm_campaign: utmCampaign,
+    });
+  }, []);
   return (
     <div className="App">
     <BrowserRouter>
