290cd09693f1f478681c5ee90cc8427b0542c856
bpdeveloperscommunity <bepracticaldeveloperscommunity@gmail.com>
2024-03-19 17:42:51 +0530

    all files

---
+++ src/components/card/Card.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './style.css'
+
+function Card() {
+  return (
+    <div className='container-fluid'>
+      <div className="row">
+        <div className="col-md-6 offset-md-3">
+            <div className="card-m">
+                <img src="https://assets.website-files.com/64b936c88eb5ddace1f89459/64baf5c1df5796b0a349d5d8_SDKs-icons-p-800.png" alt="" className='w-100' />
+            </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Card
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -6,12 +6,18 @@ import Class from '../../components/class/Class'
 import Faq from '../../components/Faq/Faq'
 import Works from '../../components/works/Works'
 import Time from '../../components/time/Time'
+import Card from '../../components/card/Card'
 
 function Home() {
   return (
     <section className='home'>
       <Hero/>
       <Marquee/>
+      <div className="book-btns">
+        <a href='https://edubepractical4623.graphy.com/sessions/What-after-degree-65f7f229c26f7353cabdcf38' className='btn'>Register Now</a>
+        <a href='https://chat.whatsapp.com/IDc5SEuahlKCVpcjjx4IJm' className='btn'>Join To whatsapp</a>
+      </div>
+      <Card/>
       <Class/>
       <Time/>
       {/* <Works/> */}
